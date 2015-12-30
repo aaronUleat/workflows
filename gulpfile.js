a0ea569289ee851190dcd8657c158983f13a0afe
Aaron Ulate <aaronuleat@gmail.com>
2015-12-30 14:12:44 -0600

    Added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -46,4 +46,10 @@ gulp.task('compass', function() {
 });
 
 
-gulp.task('all', ['coffee', 'js', 'compass']);
+gulp.task('watch', function() {
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('default', ['coffee', 'js', 'compass']);
